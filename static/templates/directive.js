aae0f015dacceceabf333a90bfbf29562955819f
Kirill <abroskink.58@gmail.com>
2025-04-06 17:55:52 +0300

    Исрпавлен шаблон rasporyajenie.docx Распоряжения, переписан directive.js

--- static/templates/directive.js
+++ static/templates/directive.js
@@ -1,7 +1,5 @@
 export const prepareData = (formData) => {
-    // Пример подготовки данных для "Распоряжения"
-    
-    // Форматируем дату в нужном формате, например:
+    // Обработка даты распоряжения
     if (formData.date) {
         const [year, month, day] = formData.date.split("-");
         formData.day = day;
@@ -9,7 +7,23 @@ export const prepareData = (formData) => {
         formData.year = year;
     }
 
-    // Преобразуем ФИО в короткий формат
+    // Обработка даты начала рекламной стратегии
+    if (formData.date_start) {
+        const [year_strat, month_strat, day_strat] = formData.date_start.split("-");
+        formData.day_strat = day_strat;
+        formData.month_strat = month_strat;
+        formData.year_strat = year_strat;  // Можно получить последний символ года для записи в шаблон
+    }
+
+    // Обработка даты завершения (для задания)
+    if (formData.date_task) {
+        const [year_task, month_task, day_task] = formData.date_task.split("-");
+        formData.day_task = day_task;
+        formData.month_task = month_task;
+        formData.year_task = year_task;
+    }
+
+    // Обработка ФИО маркетолога
     if (formData.FIO) {
         const fioParts = formData.FIO.split(' ');
         if (fioParts.length === 3) {
@@ -19,6 +33,22 @@ export const prepareData = (formData) => {
         }
     }
 
-    // Добавь здесь дополнительные преобразования данных для этого шаблона
+    // Обработка ФИО отдавшего распоряжение (директора)
+    if (formData.FIO_boss) {
+        const fioParts = formData.FIO_boss.split(' ');
+        if (fioParts.length === 3) {
+            formData.FIO_boss_short = `${fioParts[0]} ${fioParts[1][0]}. ${fioParts[2][0]}.`;
+        } else if (fioParts.length === 2) {
+            formData.FIO_boss_short = `${fioParts[0]} ${fioParts[1][0]}.`;
+        }
+    }
+
+    // Передаем все нужные данные в шаблон
+    formData.name_of_organization = formData.name_of_organization || '';
+    formData.number_of_order = formData.number_of_order || '';
+    formData.num_of_task = formData.num_of_task || '';
+    formData.city = formData.city || '';
+    formData.post_boss = formData.post_boss || '';
+
     return formData;
 };
